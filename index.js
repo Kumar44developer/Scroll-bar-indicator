bb15cf9d84210d065085a5e46edb18caff80df64
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-15 21:55:06 +0530

    Added scroll event listener to trigger scroll indicator updates

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const totalScrollHeight = document.body.scrollHeight - window.innerHeight;
 function updateScrollIndicator(){
     const scrollPercentage = (window.scrollY/totalScrollHeight) * 100;
     scrollIndicator.style.width = scrollPercentage + "%";
-}
+}window.addEventListener("scroll",updateScrollIndicator);
